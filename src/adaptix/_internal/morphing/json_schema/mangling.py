4281c54648da95ddb094f2c6f6aaef3d24bf7464
pavel <p917910@gmail.com>
2025-08-17 14:00:29 +0300

    Reimplement mangling mechanism adding deduplication

--- src/adaptix/_internal/morphing/json_schema/mangling.py
+++ src/adaptix/_internal/morphing/json_schema/mangling.py
@@ -1,10 +1,9 @@
+from collections import defaultdict
 from collections.abc import Container, Mapping, Sequence
 from itertools import count
 from typing import Optional
 
-from ...datastructures import OrderedUniqueGrouper
-from .definitions import LocalRefSource
-from .resolver import RefMangler
+from .resolver import LocalRefSourceGroup, RefMangler
 
 
 class IndexRefMangler(RefMangler):
@@ -16,16 +15,16 @@ class IndexRefMangler(RefMangler):
         self,
         occupied_refs: Container[str],
         common_ref: str,
-        sources: Sequence[LocalRefSource],
-    ) -> Mapping[LocalRefSource, str]:
+        sources_groups: Sequence[LocalRefSourceGroup],
+    ) -> Mapping[LocalRefSourceGroup, str]:
         result = {}
         counter = count(self._start)
-        for source in sources:
+        for sources_group in sources_groups:
             while True:
                 idx = next(counter)
                 mangled = self._with_index(common_ref, idx)
                 if mangled not in occupied_refs:
-                    result[source] = mangled
+                    result[sources_group] = mangled
                     break
 
         return result
@@ -39,12 +38,20 @@ class QualnameRefMangler(RefMangler):
         self,
         occupied_refs: Container[str],
         common_ref: str,
-        sources: Sequence[LocalRefSource],
-    ) -> Mapping[LocalRefSource, str]:
-        return {source: self._generate_name(source) or common_ref for source in sources}
+        sources_groups: Sequence[LocalRefSourceGroup],
+    ) -> Mapping[LocalRefSourceGroup, str]:
+        return {
+            sources_group: self._generate_name(sources_group) or common_ref
+            for sources_group in sources_groups
+        }
 
-    def _generate_name(self, source: LocalRefSource) -> Optional[str]:
-        tp = source.loc_stack.last.type
+    def _generate_name(self, sources_group: LocalRefSourceGroup) -> Optional[str]:
+        if len(sources_group.sources) > 1:
+            tps = {source.loc_stack.last.type for source in sources_group.sources}
+            if len(tps) > 1:
+                return None
+
+        tp = sources_group.sources[0].loc_stack.last.type
         return getattr(tp, "__qualname__", None)
 
 
@@ -57,16 +64,19 @@ class CompoundRefMangler(RefMangler):
         self,
         occupied_refs: Container[str],
         common_ref: str,
-        sources: Sequence[LocalRefSource],
-    ) -> Mapping[LocalRefSource, str]:
-        mangled = self._base.mangle_refs(occupied_refs, common_ref, sources)
+        sources_groups: Sequence[LocalRefSourceGroup],
+    ) -> Mapping[LocalRefSourceGroup, str]:
+        mangled = self._base.mangle_refs(occupied_refs, common_ref, sources_groups)
 
-        grouper = OrderedUniqueGrouper[str, LocalRefSource]()
-        for source, ref in mangled.items():
-            grouper.add(ref, source)
+        grouper = defaultdict(list)
+        for source_group, ref in mangled.items():
+            grouper[ref].append(source_group)
 
-        for ref, ref_sources in grouper.finalize().items():
-            if len(ref_sources) > 1:
-                mangled = {**mangled, **self._wrapper.mangle_refs(occupied_refs, ref, ref_sources)}
+        for ref, ref_sources_groups in grouper.items():
+            if len(ref_sources_groups) > 1:
+                mangled = {
+                    **mangled,
+                    **self._wrapper.mangle_refs(occupied_refs, ref, ref_sources_groups),
+                }
 
         return mangled
--- src/adaptix/_internal/morphing/json_schema/resolver.py
+++ src/adaptix/_internal/morphing/json_schema/resolver.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
 from collections import defaultdict
 from collections.abc import Container, Mapping, Sequence
+from typing import Optional, TypeVar
 
 from ...datastructures import OrderedUniqueGrouper
 from ...provider.loc_stack_filtering import LocStack
 from ...provider.loc_stack_tools import format_loc_stack
 from .definitions import JSONSchema, LocalRefSource, ResolvedJSONSchema
-from .schema_tools import replace_json_schema_ref, traverse_json_schema
+from .schema_tools import approx_hash_json_schema, replace_json_schema_ref, traverse_json_schema
 
 
 class JSONSchemaResolver(ABC):
@@ -21,6 +22,11 @@ class JSONSchemaResolver(ABC):
         ...
 
 
+class LocalRefSourceGroup:
+    def __init__(self, sources: Sequence[LocalRefSource[JSONSchema]]):
+        self.sources = sources
+
+
 class RefGenerator(ABC):
     @abstractmethod
     def generate_ref(self, json_schema: JSONSchema, loc_stack: LocStack) -> str:
@@ -33,9 +39,36 @@ class RefMangler(ABC):
         self,
         occupied_refs: Container[str],
         common_ref: str,
-        sources: Sequence[LocalRefSource],
-    ) -> Mapping[LocalRefSource, str]:
+        sources_groups: Sequence[LocalRefSourceGroup],
+    ) -> Mapping[LocalRefSourceGroup, str]:
         ...
+
+
+T = TypeVar("T")
+
+
+class _ContainerWithExtra(Container[T]):
+    __slots__ = ("_extra", "_original")
+
+    def __init__(self, original: Container[T], extra: T):
+        self._original = original
+        self._extra = extra
+
+    def __contains__(self, item):
+        return item == self._extra or item in self._original
+
+
+class CustomJSONSchemaHasher:
+    __slots__ = ("_extra", "_json_schema")
+
+    def __init__(self, json_schema: JSONSchema):
+        self._json_schema = json_schema
+
+    def __hash__(self):
+        return approx_hash_json_schema(self._json_schema)
+
+    def __eq__(self, other):
+        return self._json_schema == other._json_schema
 
 
 class BuiltinJSONSchemaResolver(JSONSchemaResolver):
@@ -81,40 +114,94 @@ class BuiltinJSONSchemaResolver(JSONSchemaResolver):
         occupied_refs: Container[str],
         ref_to_sources: Mapping[str, Sequence[LocalRefSource]],
     ) -> Mapping[LocalRefSource, str]:
-        source_determinator = {}
-        for common_ref, sources in ref_to_sources.items():
-            if len(sources) == 1 and common_ref not in occupied_refs:
-                source_determinator[sources[0]] = common_ref
-            else:
-                self._validate_sources(common_ref, sources)
-                mangling_result = self._ref_mangler.mangle_refs(occupied_refs, common_ref, sources)
-                source_determinator.update(mangling_result)
-        self._validate_mangling(source_determinator)
-        return source_determinator
+        source_group_to_ref: dict[LocalRefSourceGroup, str] = {}
 
-    def _validate_sources(self, common_ref: str, sources: Sequence[LocalRefSource]) -> None:
-        pinned_sources = [source for source in sources if source.value is not None]
-        if len(pinned_sources) > 1:
-            pinned = ", ".join(f"`{format_loc_stack(pinned.loc_stack)}`" for pinned in pinned_sources)
+        for common_ref, sources in ref_to_sources.items():
+            sources_groups = self._group_sources(sources)
+            source_group_to_ref.update(
+                self._apply_mangling(occupied_refs, common_ref, sources_groups),
+            )
+
+        self._validate_mangling(source_group_to_ref)
+        return {
+            source: ref
+            for source_group, ref in source_group_to_ref.items()
+            for source in source_group.sources
+        }
+
+    def _group_sources(self, sources: Sequence[LocalRefSource]) -> Sequence[LocalRefSourceGroup]:
+        grouper = OrderedUniqueGrouper[CustomJSONSchemaHasher, LocalRefSource]()
+        for source in sources:
+            grouper.add(CustomJSONSchemaHasher(source.json_schema), source)
+
+        return [
+            LocalRefSourceGroup(sources)
+            for _json_schema, sources in grouper.finalize().items()
+        ]
+
+    def _apply_mangling(
+        self,
+        occupied_refs: Container[str],
+        common_ref: str,
+        sources_groups: Sequence[LocalRefSourceGroup],
+    ) -> Mapping[LocalRefSourceGroup, str]:
+        if len(sources_groups) == 1 and common_ref not in occupied_refs:
+            return {sources_groups[0]: common_ref}
+
+        manglable, pinned = self._validate_pining_conflict(common_ref, sources_groups)
+        if pinned is None:
+            return self._ref_mangler.mangle_refs(occupied_refs, common_ref, manglable)
+
+        mangling_result = self._ref_mangler.mangle_refs(
+            _ContainerWithExtra(occupied_refs, common_ref), common_ref, manglable,
+        )
+        return {**mangling_result, pinned: common_ref}
+
+    def _validate_pining_conflict(
+        self,
+        common_ref: str,
+        sources_groups: Sequence[LocalRefSourceGroup],
+    ) -> tuple[Sequence[LocalRefSourceGroup], Optional[LocalRefSourceGroup]]:
+        pinned_groups = [
+            source_group
+            for source_group in sources_groups
+            if any(source for source in source_group.sources if source.value is not None)
+        ]
+        if len(pinned_groups) > 1:
+            text = ", ".join(
+                ", ".join(f"`{format_loc_stack(source.loc_stack)}`" for source in pinned.sources)
+                for pinned in pinned_groups
+            )
             raise ValueError(
                 f"Cannot create consistent json schema,"
                 f" there are different sub schemas with pinned ref {common_ref!r}."
-                f" {pinned}",
+                f" {text}",
             )
+        if len(pinned_groups) == 0:
+            return sources_groups, None
 
-    def _validate_mangling(self, source_determinator: Mapping[LocalRefSource, str]) -> None:
-        ref_to_sources = defaultdict(list)
-        for source, ref in source_determinator.items():
-            ref_to_sources[ref].append(source)
+        pinned = pinned_groups[0]
+        return list(filter(lambda x: x != pinned, sources_groups)), pinned
 
-        unmangled = [(ref, sources) for ref, sources in ref_to_sources.items() if len(sources) > 1]
+    def _validate_mangling(self, source_determinator: Mapping[LocalRefSourceGroup, str]) -> None:
+        ref_to_sources_groups = defaultdict(list)
+        for source_group, ref in source_determinator.items():
+            ref_to_sources_groups[ref].append(source_group)
+
+        unmangled = [
+            (ref, sources_groups)
+            for ref, sources_groups in ref_to_sources_groups.items()
+            if len(sources_groups) > 1
+        ]
         if unmangled:
             unmangled_desc = "; ".join(
                 f"For ref {ref!r} at "
                 + ", and at ".join(
-                    f"`{format_loc_stack(source.json_schema)}`" for source in sources
+                    f"`{format_loc_stack(source.loc_stack)}`"
+                    for source_group in sources_groups
+                    for source in source_group.sources
                 )
-                for ref, sources in unmangled
+                for ref, sources_groups in unmangled
             )
             raise ValueError(
                 f"Cannot create consistent json schema,"
